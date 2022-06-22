73ae1853ab7c8a608b5192dbd55b53cf2c82a417
a8hay <a8haysingh@gmail.com>
2022-06-22 15:57:52 +0530

    User Entity completed with api protection

--- microservice/routes/userRoutes.js
+++ microservice/routes/userRoutes.js
@@ -1,8 +1,14 @@
 import express from 'express'
-import { getMe } from '../controllers/userController.js'
+import {
+  registerUser,
+  loginUser,
+  getMe,
+} from '../controllers/userController.js'
 
 const userRouter = express.Router()
 
+userRouter.post('/', registerUser)
+userRouter.post('/login', loginUser)
 userRouter.get('/getMe', getMe)
 
 export default userRouter
--- microservice/server.js
+++ microservice/server.js
@@ -3,12 +3,19 @@ import 'colors'
 import { connectDB } from './config/db.js'
 import express from 'express'
 import userRouter from './routes/userRoutes.js'
+import errorHandler from './middlewares/errorMiddleware.js'
+import protect from './middlewares/authMiddleware.js'
 const PORT = process.env.PORT || 5000
 
 connectDB()
 const app = express()
 
-app.use('/api/user', userRouter)
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/api/user', protect, userRouter)
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
   console.log(`Server started on PORT ${PORT}`.cyan.underline)
